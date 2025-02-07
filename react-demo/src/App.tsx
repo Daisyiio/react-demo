1a63407c12fa71955ea94fac88300d66a71c83f3
邹钰杰 <554091877@qq.com>
2025-02-07 20:15:49 +0800

    关于prop传值

--- react-demo/src/App.tsx
+++ react-demo/src/App.tsx
@@ -3,6 +3,7 @@ import "./App.scss";
 import Mustache from "@/components/Mustache";
 import { UseState, UseState2, UseState3 } from "@/components/UseState";
 import { UseReducer1, UserReducer2 } from "@/components/useReducer";
+import {PropAndSlot} from '@/components/PropAndSlot/PropAndSlot'
 function App() {
   return (
     <>
@@ -13,6 +14,7 @@ function App() {
         <UseState3></UseState3>
         <UseReducer1></UseReducer1>
         <UserReducer2></UserReducer2>
+        <PropAndSlot></PropAndSlot>
       </div>
     </>
   );
